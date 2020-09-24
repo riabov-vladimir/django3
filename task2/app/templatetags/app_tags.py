19f1c47100d9cff28f165c4bf297170861dd1176
riabow <63100171+riabov-vladimir@users.noreply.github.com>
2020-09-24 11:55:38 +0300

    task 2 - done

--- task2/app/templatetags/app_tags.py
+++ task2/app/templatetags/app_tags.py
@@ -5,61 +5,6 @@ register = template.Library()
 
 
 @register.filter
-def highlight_current(value):
-
-	if value == 'app/contacts.html':
-		return '''
-		  <nav class="row">
-			<div class="nav-wrapper col s12">
-			  <a href="{% url 'home' %}" class="brand-logo">
-			  Компьютеры</a>
-			  <ul class="right hide-on-med-and-down">
-				<li><a href="{% url 'examples' %}">Примеры</a></li>
-				<li class="active"><a href="{% url 'contacts' %}">Контакты</a></li>
-				<li><a href="{% url 'about' %}">О проекте</a></li>
-			  </ul>
-			</div>
-		  </nav>
-		'''
-	elif value == 'app/about.html':
-		return '''
-		  <nav class="row">
-			<div class="nav-wrapper col s12">
-			  <a href="{% url 'home' %}" class="brand-logo">
-			  Компьютеры</a>
-			  <ul class="right hide-on-med-and-down">
-				<li><a href="{% url 'examples' %}">Примеры</a></li>
-				<li><a href="{% url 'contacts' %}">Контакты</a></li>
-				<li class="active"><a href="{% url 'about' %}">О проекте</a></li>
-			  </ul>
-			</div>
-		  </nav>
-		'''
-	elif value == 'app/examples.html':
-		return '''
-		  <nav class="row">
-			<div class="nav-wrapper col s12">
-			  <a href="{% url 'home' %}" class="brand-logo">
-			  Компьютеры</a>
-			  <ul class="right hide-on-med-and-down">
-				<li class="active"><a href="{% url 'examples' %}">Примеры</a></li>
-				<li><a href="{% url 'contacts' %}">Контакты</a></li>
-				<li><a href="{% url 'about' %}">О проекте</a></li>
-			  </ul>
-			</div>
-		  </nav>
-				'''
-	elif value == 'app/home.html':
-		return '''
-		  <nav class="row">
-			<div class="nav-wrapper col s12">
-			  <a href="{% url 'home' %}" class="brand-logo">
-			  Компьютеры</a>
-			  <ul class="right hide-on-med-and-down">
-				<li><a href="{% url 'examples' %}">Примеры</a></li>
-				<li><a href="{% url 'contacts' %}">Контакты</a></li>
-				<li><a href="{% url 'about' %}">О проекте</a></li>
-			  </ul>
-			</div>
-		  </nav>
-		'''
+def is_active(value, arg):
+	if value == arg:
+		return 'active'
