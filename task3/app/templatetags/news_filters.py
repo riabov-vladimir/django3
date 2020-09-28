785c60ece40c114a383dd48faeae7232bcc41813
riabow <63100171+riabov-vladimir@users.noreply.github.com>
2020-09-28 18:25:23 +0300

    final

--- task3/app/templatetags/news_filters.py
+++ task3/app/templatetags/news_filters.py
@@ -56,27 +56,3 @@ def format_selftext(value: str, count) -> str:
     right = ' '.join(text_split[-count:])
 
     return f'{left}  . . .  {right}'
-
-
-"""
-
-форматирование поля score (название на ваше усмотрение):
-Рейтинг меньше -5, пишет "все плохо"
-Рейтинг от -5 до 5 – "нейтрально"
-Рейтинг больше 5 – "хорошо"
-Если поле score отсутствует, то рендерится дефолтное значение, которое передается в качестве параметра фильтра.
-
-format_selftext:
-Оставляет count первых и count последних слов, между ними должно быть троеточие.
- count задается параметром фильтра. Пример c count = 5: "Hi all sorry if this ... help or advice greatly appreciated."
-
-Знаки препинания остаются, обрезаются только слова.
-"""
-
-if __name__ == '__main__':
-    txt = "Discussion. of using, Python in a professional environment, getting jobs in Python and more! **This thread is not for recruitment, please see** r/PythonJobs **or the thread in the sidebar for that.**"
-    txt_l = txt.split()  #[5:]
-    # x = txt.rpartition("Python")
-    ll = len(txt_l)
-    print(txt_l[:5])
-    print(txt_l)
